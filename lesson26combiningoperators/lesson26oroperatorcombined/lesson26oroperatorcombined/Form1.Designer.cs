cd7aa2639e0257a20bee5687baba464ff01afa2b
Asif-packt <35103806+Asif-packt@users.noreply.github.com>
2019-05-06 18:27:51 +0530

    Code Updated

---
+++ lesson26combiningoperators/lesson26oroperatorcombined/lesson26oroperatorcombined/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace lesson26oroperatorcombined
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rchInputBox = new System.Windows.Forms.RichTextBox();
+            this.btnFindVowels = new System.Windows.Forms.Button();
+            this.rchVowelsBox = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // rchInputBox
+            // 
+            this.rchInputBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F);
+            this.rchInputBox.Location = new System.Drawing.Point(13, 13);
+            this.rchInputBox.Name = "rchInputBox";
+            this.rchInputBox.Size = new System.Drawing.Size(922, 116);
+            this.rchInputBox.TabIndex = 0;
+            this.rchInputBox.Text = "";
+            // 
+            // btnFindVowels
+            // 
+            this.btnFindVowels.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F);
+            this.btnFindVowels.Location = new System.Drawing.Point(12, 153);
+            this.btnFindVowels.Name = "btnFindVowels";
+            this.btnFindVowels.Size = new System.Drawing.Size(922, 81);
+            this.btnFindVowels.TabIndex = 1;
+            this.btnFindVowels.Text = "FIND THE VOWELS";
+            this.btnFindVowels.UseVisualStyleBackColor = true;
+            this.btnFindVowels.Click += new System.EventHandler(this.btnFindVowels_Click);
+            // 
+            // rchVowelsBox
+            // 
+            this.rchVowelsBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F);
+            this.rchVowelsBox.Location = new System.Drawing.Point(13, 259);
+            this.rchVowelsBox.Name = "rchVowelsBox";
+            this.rchVowelsBox.Size = new System.Drawing.Size(922, 450);
+            this.rchVowelsBox.TabIndex = 2;
+            this.rchVowelsBox.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(947, 736);
+            this.Controls.Add(this.rchVowelsBox);
+            this.Controls.Add(this.btnFindVowels);
+            this.Controls.Add(this.rchInputBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox rchInputBox;
+        private System.Windows.Forms.Button btnFindVowels;
+        private System.Windows.Forms.RichTextBox rchVowelsBox;
+    }
+}
+
---
+++ lesson26combiningoperators/lesson26oroperatorcombined/lesson26oroperatorcombined/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace lesson26oroperatorcombined
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnFindVowels_Click(object sender, EventArgs e)
+        {
+            rchVowelsBox.Text = "";//clears box on each button click so output does not grow
+            string text = rchInputBox.Text.ToLower();//grab the text, convert to lower case and save to variable named text
+            //in English, the basic vowels are : a, e,i,o,u
+            //grab each character, compare it against a,e,i,o, u to see whether it's a vowel
+            foreach(char c in text)//needed to grab each character and store it in c for each cycle of the loop
+            {
+                //check whether the character that c represents on each cycle happens to be one of the vowels
+                if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u')
+                    rchVowelsBox.Text += $"{c}\n";
+            }
+        }
+    }
+}
