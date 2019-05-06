c27fa04ae0254148e1728dbf7afd414884a4b935
Asif-packt <35103806+Asif-packt@users.noreply.github.com>
2019-05-06 18:27:49 +0530

    Code Updated

---
+++ lesson24logicalandoperator/lesson24ampersand/temp/Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace temp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.actionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.addToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.subtractToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.multiplyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.chkExercise = new System.Windows.Forms.CheckBox();
+            this.chkDiet = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lblResults = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.actionToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(880, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // actionToolStripMenuItem
+            // 
+            this.actionToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.addToolStripMenuItem,
+            this.subtractToolStripMenuItem,
+            this.multiplyToolStripMenuItem});
+            this.actionToolStripMenuItem.Name = "actionToolStripMenuItem";
+            this.actionToolStripMenuItem.Size = new System.Drawing.Size(54, 20);
+            this.actionToolStripMenuItem.Text = "Action";
+            // 
+            // addToolStripMenuItem
+            // 
+            this.addToolStripMenuItem.Name = "addToolStripMenuItem";
+            this.addToolStripMenuItem.Size = new System.Drawing.Size(138, 22);
+            this.addToolStripMenuItem.Text = "Up by 2\'s";
+            // 
+            // subtractToolStripMenuItem
+            // 
+            this.subtractToolStripMenuItem.Name = "subtractToolStripMenuItem";
+            this.subtractToolStripMenuItem.Size = new System.Drawing.Size(138, 22);
+            this.subtractToolStripMenuItem.Text = "Down by 3\'s";
+            // 
+            // multiplyToolStripMenuItem
+            // 
+            this.multiplyToolStripMenuItem.Name = "multiplyToolStripMenuItem";
+            this.multiplyToolStripMenuItem.Size = new System.Drawing.Size(138, 22);
+            this.multiplyToolStripMenuItem.Text = "Multiply";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F);
+            this.label1.Location = new System.Drawing.Point(12, 185);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 63);
+            this.label1.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F);
+            this.label2.Location = new System.Drawing.Point(-2, 234);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 63);
+            this.label2.TabIndex = 6;
+            // 
+            // chkExercise
+            // 
+            this.chkExercise.AutoSize = true;
+            this.chkExercise.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F);
+            this.chkExercise.Location = new System.Drawing.Point(12, 51);
+            this.chkExercise.Name = "chkExercise";
+            this.chkExercise.Size = new System.Drawing.Size(254, 67);
+            this.chkExercise.TabIndex = 7;
+            this.chkExercise.Text = "Exercise";
+            this.chkExercise.UseVisualStyleBackColor = true;
+            // 
+            // chkDiet
+            // 
+            this.chkDiet.AutoSize = true;
+            this.chkDiet.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F);
+            this.chkDiet.Location = new System.Drawing.Point(12, 124);
+            this.chkDiet.Name = "chkDiet";
+            this.chkDiet.Size = new System.Drawing.Size(143, 67);
+            this.chkDiet.TabIndex = 8;
+            this.chkDiet.Text = "Diet";
+            this.chkDiet.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F);
+            this.button1.Location = new System.Drawing.Point(272, 80);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(585, 91);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Check Discounts";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblResults
+            // 
+            this.lblResults.AutoSize = true;
+            this.lblResults.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F);
+            this.lblResults.Location = new System.Drawing.Point(23, 234);
+            this.lblResults.Name = "lblResults";
+            this.lblResults.Size = new System.Drawing.Size(0, 63);
+            this.lblResults.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(880, 466);
+            this.Controls.Add(this.lblResults);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.chkDiet);
+            this.Controls.Add(this.chkExercise);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem actionToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem addToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem subtractToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ToolStripMenuItem multiplyToolStripMenuItem;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox chkExercise;
+        private System.Windows.Forms.CheckBox chkDiet;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lblResults;
+    }
+}
+
---
+++ lesson24logicalandoperator/lesson24ampersand/temp/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace temp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            //if both conditions on both sides of the && are true, the code immediately following the if runs
+            //the logical && short circuits, so if the left side is false, the right side is not checked 
+            // and the code immediately following the if is skipped
+            if(chkExercise.Checked && chkDiet.Checked)//checks if both boxes are checked
+            {
+                lblResults.Text = "10% discount!"; //runs when both boxes are checked
+            }
+            else if(chkExercise.Checked)//checks if only the exercise box is checked
+            {
+                lblResults.Text = "4% discount!";//runs when only the exercise box is checked
+            }
+            else if(chkDiet.Checked)//checks if only the diet box is checked
+            {
+                lblResults.Text = "6% discount!";//runs when only the diet box is checked
+            }
+            else
+            {
+                lblResults.Text = "";//runs when neither box is checked, that is, when both boxes are unchecked
+            }
+        }
+    }
+}
