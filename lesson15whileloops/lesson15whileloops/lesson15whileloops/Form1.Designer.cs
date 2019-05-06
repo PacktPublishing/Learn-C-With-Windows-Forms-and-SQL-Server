420c6c71e814f60dcfbfc951f9747a08f00fef13
Asif-packt <35103806+Asif-packt@users.noreply.github.com>
2019-05-06 18:24:03 +0530

    Code Updated

---
+++ lesson15whileloops/lesson15whileloops/lesson15whileloops/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace lesson15whileloops
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F);
+            this.radioButton1.Location = new System.Drawing.Point(13, 13);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(404, 67);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.Text = "Show 1,2,3,4,5";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F);
+            this.radioButton2.Location = new System.Drawing.Point(13, 98);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(404, 67);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.Text = "Show 5,4,3,2,1";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F);
+            this.richTextBox1.Location = new System.Drawing.Point(13, 182);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(536, 531);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(561, 725);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ lesson15whileloops/lesson15whileloops/lesson15whileloops/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace lesson15whileloops
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            richTextBox1.Text = "";//clears the rich text box on each click
+            //print 1,2,3,4,5
+            //values begin at 1, values increase by 1, last value is 5
+            //int is the data type like -10,0, 50, not 1.353
+            int i = 1;//list begins at 1, so type int i=1
+            //i will assume the values: 1,2,3,4,5
+            //i<=5 because as loop runs, i=1, i=2,i=3,i=4,i=5
+            //when i=6,6<=5, so the loop is skipped
+            while(i<=5)
+            {
+                richTextBox1.Text += $"i={i}\n";
+                i++;//this is needed to grow the value of i on each cycle of the loop
+            }
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            richTextBox1.Text = "";//this clears the text box on every click of the radio button
+            //list the values 5,4,3,2,1
+            //list starts at 5, goes down by 1, ends on 1
+            int i = 5;//because the list starts at 5, set the value of i to 5
+            //i=5, i=4,i=3,i=2,i=1, each of the previous makes the loop run
+            //i=0, 0>=1 fails, so loop code does not run again
+            while(i>=1)
+            {
+                richTextBox1.Text += $"i={i}\n";
+                i--;//this has the effect of decreasing the value of i on each cycle of the loop
+            }
+        }
+    }
+}
