1ea7bc1d9b6469abb3ef392d1e24ceb3a73d76d8
Asif-packt <35103806+Asif-packt@users.noreply.github.com>
2019-05-06 18:23:04 +0530

    Code Updated

---
+++ lesson8mathwithtwo/lesson8mathwith2variables/lesson8averages/lesson7readconvert/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lesson7readconvert
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            //"10" as a string can be converted into a numerical version without double quotes: 10
+            //if input is in the form "ten", this will crash the application
+            //double.Parse(textBox1.Text): grab the text and convert, if possible
+            // to numerical form for processing
+            double input = double.Parse(textBox1.Text);
+            //to display 10% of the input, write 0.1*input
+            //input of 10, 0.1*10=1
+            //input of 100, 0.1*100=10
+            label1.Text = $"10% of {input} is {0.1*input}";
+        }
+    }
+}
---
+++ lesson8mathwithtwo/lesson8mathwith2variables/lesson8averages/lesson8averages/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lesson8averages
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //add values, divide by 2
+            //(2+5)/2
+            //first read the values, convert to numerical, produce average
+            //read text, convert to numerical form and save to the variables
+            decimal firstValue = decimal.Parse(textBox1.Text);
+            decimal secondValue = decimal.Parse(textBox2.Text);
+            //average is called a computed variable
+            //operand operator operand
+            //first add the values, divide by 2, and save to average values
+            decimal average = (firstValue + secondValue) / 2;
+            //:C has the effect of formatting average value as currency
+            label1.Text = $"Average Salary:{average:C}";
+        }
+    }
+}
