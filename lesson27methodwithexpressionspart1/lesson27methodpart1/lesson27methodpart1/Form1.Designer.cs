b9f46f287de212bb75f744cbc543c38d48fa3262
Asif-packt <35103806+Asif-packt@users.noreply.github.com>
2019-05-06 18:29:09 +0530

    Code Updated

---
+++ lesson27methodwithexpressionspart1/lesson27methodpart1/lesson27methodpart1/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace lesson27methodpart1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.toolTip2 = new System.Windows.Forms.ToolTip(this.components);
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(13, 13);
+            this.trackBar1.Minimum = -10;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(1298, 45);
+            this.trackBar1.TabIndex = 0;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.Location = new System.Drawing.Point(13, 64);
+            this.trackBar2.Minimum = -10;
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Size = new System.Drawing.Size(1298, 45);
+            this.trackBar2.TabIndex = 1;
+            this.trackBar2.Scroll += new System.EventHandler(this.trackBar2_Scroll);
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 35F);
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Items.AddRange(new object[] {
+            "Add(this is at index 0)",
+            "Divide(this is at index 1)"});
+            this.checkedListBox1.Location = new System.Drawing.Point(24, 116);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(547, 114);
+            this.checkedListBox1.TabIndex = 2;
+            this.checkedListBox1.SelectedIndexChanged += new System.EventHandler(this.checkedListBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F);
+            this.label1.Location = new System.Drawing.Point(24, 276);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 63);
+            this.label1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1323, 528);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.checkedListBox1);
+            this.Controls.Add(this.trackBar2);
+            this.Controls.Add(this.trackBar1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.TrackBar trackBar2;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.ToolTip toolTip2;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ lesson27methodwithexpressionspart1/lesson27methodpart1/lesson27methodpart1/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace lesson27methodpart1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            //as you move the marker, display a tool tip with the current value of the track bar
+            toolTip1.SetToolTip(trackBar1, trackBar1.Value.ToString());
+        }
+
+        private void trackBar2_Scroll(object sender, EventArgs e)
+        {
+            //as you move the marker, display a tool tip with the current value of the track bar
+            toolTip2.SetToolTip(trackBar2, trackBar2.Value.ToString());
+        }
+
+        private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //switch blocks are used to make decisions by allowing us to examine the different value of a variable
+            //in this case, it's the index of the entry selected in the 
+            switch(checkedListBox1.SelectedIndex)//use to examine selected index of entry in box
+            {
+                case 0: //case label that corresponds to first item selected, or Add
+                    label1.Text = Add(trackBar1.Value,trackBar2.Value).ToString();//Call Add down below
+                    break;
+                case 1://examine whether the word Divide has been selected
+                    label1.Text = Divide(trackBar1.Value,trackBar2.Value).ToString();//Call Divide down below
+                    break;
+            }
+        }
+        //private means accessible only in our code
+        //static means we will be able to call our method just by typing its name
+        //double is the type of data returned by the method
+        //name of Method is Add
+        //double x and double y are parameters
+        //=> is called "expression bodied member" notation
+        private static double Add(double x, double y) => x + y;
+
+        private static double Divide(double x, double y)
+        {
+            //in math, division by 0 is not allowed: 5/0, for example, is not allowed
+            //use ternary operator to handle division by 0 safely
+            //if y is not 0, divide x by y and send that back to the calling code
+            //if y is 0, return 0.0 to the calling code because division by 0 is not defined
+            return (y != 0) ? x / y : 0.0;
+        }
+    }
+}
