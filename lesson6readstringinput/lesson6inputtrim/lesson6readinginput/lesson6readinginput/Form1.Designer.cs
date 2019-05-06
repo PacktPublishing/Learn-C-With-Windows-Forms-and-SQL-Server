7ba921c4538776e1ae231d0324c1ae0be9418a4a
Asif-packt <35103806+Asif-packt@users.noreply.github.com>
2019-05-06 18:22:34 +0530

    Code Updated

---
+++ lesson6readstringinput/lesson6inputtrim/lesson6readinginput/lesson6readinginput/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace lesson6readinginput
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtInput
+            // 
+            this.txtInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtInput.Location = new System.Drawing.Point(12, 33);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(1273, 62);
+            this.txtInput.TabIndex = 0;
+            this.txtInput.TextChanged += new System.EventHandler(this.txtInput_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 125);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 55);
+            this.label1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1297, 300);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtInput);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ lesson6readstringinput/lesson6inputtrim/lesson6readinginput/lesson6readinginput/Form1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace lesson6readinginput
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void txtInput_TextChanged(object sender, EventArgs e)
+        {
+            //txtInput.Text means grab the text from the text box
+            //txtInput.Text.ToLower() means convert the text value to lower case
+            //equal sign saves it to the label text property
+            //txtInput.Text.ToLower().Trim() trims aways blanks on both ends of the input
+            //data type on the left matches data type from the last method on the right
+            label1.Text = txtInput.Text.ToLower().Trim();
+        }
+    }
+}
