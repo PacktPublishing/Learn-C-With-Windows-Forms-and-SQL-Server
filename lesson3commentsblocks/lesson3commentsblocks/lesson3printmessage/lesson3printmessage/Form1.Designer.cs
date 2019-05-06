3b25435698d5ca7d2f0bc0a13f3f982fecf6f5c1
Asif-packt <35103806+Asif-packt@users.noreply.github.com>
2019-05-06 18:20:13 +0530

    Code Update

---
+++ lesson3commentsblocks/lesson3commentsblocks/lesson3printmessage/lesson3printmessage/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace lesson3printmessage
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.msgLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // msgLabel
+            // 
+            this.msgLabel.AutoSize = true;
+            this.msgLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.msgLabel.Location = new System.Drawing.Point(167, 76);
+            this.msgLabel.Name = "msgLabel";
+            this.msgLabel.Size = new System.Drawing.Size(0, 108);
+            this.msgLabel.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(958, 279);
+            this.Controls.Add(this.msgLabel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label msgLabel;
+    }
+}
+
---
+++ lesson3commentsblocks/lesson3commentsblocks/lesson3printmessage/lesson3printmessage/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace lesson3printmessage
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //this code loads when the form first loads
+            //msgLabel is the name of our label object
+            //msgLabel.Text will allow us to set the text property of the label
+            //= is the assignment operator that saves "hello, world" to the text property of the label
+            //"Hello, World" is a string because it's characters between double quotes
+            //; terminates the statement, without ;, the code will not work
+            msgLabel.Text = "Hello, World!";
+
+        }
+    }
+}
