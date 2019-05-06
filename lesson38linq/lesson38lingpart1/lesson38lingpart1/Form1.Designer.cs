f1f343592daf69a9be5bb4de1ef7cf3d1a479fa3
Asif-packt <35103806+Asif-packt@users.noreply.github.com>
2019-05-06 18:32:11 +0530

    Code Updated

---
+++ lesson38linq/lesson38lingpart1/lesson38lingpart1/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace lesson38lingpart1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.richTextBox1.Location = new System.Drawing.Point(13, 13);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(984, 239);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.richTextBox2.Location = new System.Drawing.Point(13, 271);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(984, 380);
+            this.richTextBox2.TabIndex = 1;
+            this.richTextBox2.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1009, 682);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+    }
+}
+
---
+++ lesson38linq/lesson38lingpart1/lesson38lingpart1/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace lesson38lingpart1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            richTextBox2.Text = "";//clear bottom box of text
+            //line below is used to separate the text by different characters
+            var words = richTextBox1.Text.Split(new char[] { ' ', ',', '.','!' });
+            //grab each word, check whether it's 5 or more in length, and keep track of the count
+            var count = words.Count(word => word.Length >= 5);
+            //line below shows count of words 5 or more characters in length
+            richTextBox2.Text = $"Number of words 5 or more chars:{count}";
+
+            richTextBox2.Text += "\nWords 5 or more chars. sorted:";
+            //line below picks out words that are 5 or more chars, turns the result into a list and saves to 
+            //longWords
+            var longWords = words.Where(word => word.Length >= 5).ToList();
+            //line below orders the words from shorted to longest
+            var longWordsSorted = longWords.OrderBy(word => word.Length).ToList();
+            //grab each word from the longWordsSorted list, and display it to the rich text box on the bottom
+            longWordsSorted.ForEach(word => richTextBox2.Text += $"\n{word}");
+          
+        }
+    }
+}
