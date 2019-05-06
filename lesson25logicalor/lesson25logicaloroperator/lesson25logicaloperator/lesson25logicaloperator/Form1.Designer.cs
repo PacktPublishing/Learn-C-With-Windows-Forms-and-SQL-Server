945662b0df6a935cf7522862636b86196071de75
Asif-packt <35103806+Asif-packt@users.noreply.github.com>
2019-05-06 18:27:52 +0530

    Code Updated

---
+++ lesson25logicalor/lesson25logicaloroperator/lesson25logicaloperator/lesson25logicaloperator/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace lesson25logicaloperator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F);
+            this.dateTimePicker1.Location = new System.Drawing.Point(12, 12);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(868, 68);
+            this.dateTimePicker1.TabIndex = 0;
+            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F);
+            this.label1.Location = new System.Drawing.Point(12, 108);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 63);
+            this.label1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1008, 261);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ lesson25logicalor/lesson25logicaloroperator/lesson25logicaloperator/lesson25logicaloperator/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace lesson25logicaloperator
+{
+    public partial class Form1 : Form
+    {
+        //convert the string "11/25/2016" into a date time object 
+        //it's placed here so it happens only once
+        DateTime dt = DateTime.Parse("11/25/2016");
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void dateTimePicker1_ValueChanged_1(object sender, EventArgs e)
+        {
+            //if somebody chooses a date earlier than 11/25/2016, display a discount message
+            //if somebody chooses a date later than 11/25/2016, display a discount message
+            //if somebody chooses the date itself, display no discount for your message
+            //dateTimePicker1.Value.Date<dt means if chosen date is earlier in time than 11/25/2016
+            //dateTimePicker1.Value.Date>dt means if chosen date is later in time than 11/25/2016
+            //logical or operator || because it helps us make a decision 
+            //|| returns true if the left side is true, or the right side is true, or perhaps both sides are true
+            //if the left side is true, the right is not even checked, so the code is more efficient
+            if (dateTimePicker1.Value.Date<dt || dateTimePicker1.Value.Date>dt)
+            {
+                label1.Text = "You qualify for a discount!"; //runs when if is true
+            }
+            else
+            {
+                label1.Text = "No discount for you!"; //runs when 11/25/2016 itself is selected
+            }
+        }
+    }
+}
