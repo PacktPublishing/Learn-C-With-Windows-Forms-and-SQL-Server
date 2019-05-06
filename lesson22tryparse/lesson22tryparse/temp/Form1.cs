276fd1e3e9edf6ce672b2132eeccdb360ecbfeca
Asif-packt <35103806+Asif-packt@users.noreply.github.com>
2019-05-06 18:26:53 +0530

    Code Updated

---
+++ lesson22tryparse/lesson22tryparse/temp/Form1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace temp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_DoubleClick(object sender, EventArgs e)
+        {
+            DateTime date;//declare a variable to hold the input from the box after a conversion
+            //read the value from the box, if conversion is successful, set the date variable 
+            //save the date variable as a long date string
+            //if conversion fails, save "Cannot convert" to dateFinal
+            string dateFinal = DateTime.TryParse(textBox1.Text, out date) ? date.ToLongDateString() : date.ToLongDateString();
+            label1.Text = dateFinal;//display message to user to let him or her know what's going one
+        }
+    }
+}
