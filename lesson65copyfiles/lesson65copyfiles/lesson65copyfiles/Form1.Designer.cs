cafb89fe2ffdfee38a9956cb2484f7309b7c0674
Asif-packt <35103806+Asif-packt@users.noreply.github.com>
2019-05-06 18:37:56 +0530

    Code Updated

---
+++ lesson65copyfiles/lesson65copyfiles/lesson65copyfiles/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace lesson65copyfiles
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnChooseFiles = new System.Windows.Forms.Button();
+            this.chkAll = new System.Windows.Forms.CheckBox();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.btnCopy = new System.Windows.Forms.Button();
+            this.btnChooseFolder = new System.Windows.Forms.Button();
+            this.lblPath = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.SuspendLayout();
+            // 
+            // btnChooseFiles
+            // 
+            this.btnChooseFiles.Location = new System.Drawing.Point(13, 13);
+            this.btnChooseFiles.Name = "btnChooseFiles";
+            this.btnChooseFiles.Size = new System.Drawing.Size(75, 23);
+            this.btnChooseFiles.TabIndex = 0;
+            this.btnChooseFiles.Text = "Choose Files";
+            this.btnChooseFiles.UseVisualStyleBackColor = true;
+            this.btnChooseFiles.Click += new System.EventHandler(this.btnChooseFiles_Click);
+            // 
+            // chkAll
+            // 
+            this.chkAll.AutoSize = true;
+            this.chkAll.Enabled = false;
+            this.chkAll.Location = new System.Drawing.Point(94, 19);
+            this.chkAll.Name = "chkAll";
+            this.chkAll.Size = new System.Drawing.Size(71, 17);
+            this.chkAll.TabIndex = 1;
+            this.chkAll.Text = "Check All";
+            this.chkAll.UseVisualStyleBackColor = true;
+            this.chkAll.CheckedChanged += new System.EventHandler(this.chkAll_CheckedChanged);
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(13, 43);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(460, 499);
+            this.checkedListBox1.TabIndex = 2;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(13, 548);
+            this.progressBar1.Minimum = 1;
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(460, 23);
+            this.progressBar1.Step = 1;
+            this.progressBar1.TabIndex = 3;
+            this.progressBar1.Value = 1;
+            // 
+            // btnCopy
+            // 
+            this.btnCopy.Enabled = false;
+            this.btnCopy.Location = new System.Drawing.Point(477, 13);
+            this.btnCopy.Name = "btnCopy";
+            this.btnCopy.Size = new System.Drawing.Size(75, 23);
+            this.btnCopy.TabIndex = 4;
+            this.btnCopy.Text = "Copy";
+            this.btnCopy.UseVisualStyleBackColor = true;
+            this.btnCopy.Click += new System.EventHandler(this.btnCopy_Click);
+            // 
+            // btnChooseFolder
+            // 
+            this.btnChooseFolder.Location = new System.Drawing.Point(614, 15);
+            this.btnChooseFolder.Name = "btnChooseFolder";
+            this.btnChooseFolder.Size = new System.Drawing.Size(111, 23);
+            this.btnChooseFolder.TabIndex = 5;
+            this.btnChooseFolder.Text = "Choose Folder";
+            this.btnChooseFolder.UseVisualStyleBackColor = true;
+            this.btnChooseFolder.Click += new System.EventHandler(this.btnChooseFolder_Click);
+            // 
+            // lblPath
+            // 
+            this.lblPath.AutoSize = true;
+            this.lblPath.Location = new System.Drawing.Point(614, 45);
+            this.lblPath.Name = "lblPath";
+            this.lblPath.Size = new System.Drawing.Size(0, 13);
+            this.lblPath.TabIndex = 6;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.Filter = "All Files (*.*)|*.*";
+            this.openFileDialog1.Multiselect = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(927, 649);
+            this.Controls.Add(this.lblPath);
+            this.Controls.Add(this.btnChooseFolder);
+            this.Controls.Add(this.btnCopy);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.checkedListBox1);
+            this.Controls.Add(this.chkAll);
+            this.Controls.Add(this.btnChooseFiles);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnChooseFiles;
+        private System.Windows.Forms.CheckBox chkAll;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button btnCopy;
+        private System.Windows.Forms.Button btnChooseFolder;
+        private System.Windows.Forms.Label lblPath;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+    }
+}
+
---
+++ lesson65copyfiles/lesson65copyfiles/lesson65copyfiles/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;//needed for file operations like copying
+
+namespace lesson65copyfiles
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnChooseFiles_Click(object sender, EventArgs e)
+        {
+            if(openFileDialog1.ShowDialog()==DialogResult.OK)//check whether some files are selected
+            {
+                for (int i = 0; i < openFileDialog1.FileNames.Length; i++)//loop to iterate through the array of file names
+                    checkedListBox1.Items.Add(openFileDialog1.FileNames[i]);//line actually fills the list box 
+
+                chkAll.Enabled = true;//after the list box is filled, enable the check all check box
+            }
+
+        }
+
+        private void btnChooseFolder_Click(object sender, EventArgs e)
+        {
+            if(folderBrowserDialog1.ShowDialog()==DialogResult.OK)
+            {
+                lblPath.Text = folderBrowserDialog1.SelectedPath;//display path to user of program where files will go
+                btnCopy.Enabled = true;//enable button to copy files
+            }
+        }
+
+        private void btnCopy_Click(object sender, EventArgs e)
+        {
+            progressBar1.Value = progressBar1.Minimum;//restore the progress bar between copy operations
+            progressBar1.Maximum = checkedListBox1.CheckedItems.Count;
+            foreach(var item in checkedListBox1.CheckedItems)//needed to grab each item from the list
+            {
+                progressBar1.PerformStep();//update the progress
+                //Path.GetFile gets the file name only
+                //Path.Combine creates a new path where the file is saved
+                //Copy operation is performed
+                File.Copy(item.ToString(), Path.Combine(folderBrowserDialog1.SelectedPath, Path.GetFileName(item.ToString())),true);
+            }
+
+        }
+
+        private void chkAll_CheckedChanged(object sender, EventArgs e)
+        {
+            if(chkAll.Checked)
+            {
+                for (int i = 0; i < checkedListBox1.Items.Count; i++)//needed to grab each box inside the check list box
+                    checkedListBox1.SetItemChecked(i, true);//check the box at index i
+            }
+            else
+            {
+                for (int i = 0; i < checkedListBox1.Items.Count; i++)//needed to grab each box inside the check list box
+                    checkedListBox1.SetItemChecked(i, false);//false value indicates each item in the box should be unchcked
+            }
+        }
+    }
+}
