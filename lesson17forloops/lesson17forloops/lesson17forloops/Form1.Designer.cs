aafb20f6d11ee5da7d2190ac75f7cd74878e34aa
Asif-packt <35103806+Asif-packt@users.noreply.github.com>
2019-05-06 18:25:09 +0530

    Code Updated

---
+++ lesson17forloops/lesson17forloops/lesson17forloops/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace lesson17forloops
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(13, 13);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(904, 105);
+            this.progressBar1.Step = 1;
+            this.progressBar1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F);
+            this.label1.Location = new System.Drawing.Point(2, 142);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(99, 63);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Fill";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F);
+            this.label2.Location = new System.Drawing.Point(339, 142);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(168, 63);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Reset";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F);
+            this.richTextBox1.Location = new System.Drawing.Point(13, 226);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(904, 434);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(929, 693);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.progressBar1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ lesson17forloops/lesson17forloops/lesson17forloops/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace lesson17forloops
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            //i is a variable, so its values will change from 0 to 1, 1 to 2, 2 to 3...all the way to 100
+            //begin with i=progressBar1.Minimum
+            //i<=progressBar1.Maximum ensures i goes all the way to 100
+            //i++ is present to grow the value of i by 1 on each cycle of the loop
+            for(int i=progressBar1.Minimum;i<=progressBar1.Maximum;i++)
+            {
+                richTextBox1.Text += progressBar1.Value + "\n";
+                progressBar1.PerformStep();//this has the action of filling the bar from left to right
+                richTextBox1.Text += $"{i}\n";//fills box with the values of i as loop operates
+            }
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = "";//this clears the text from the rich text box
+            progressBar1.Value = 0;//clears progress bar back to 0 so it's not green anymore
+        }
+    }
+}
