975857a93ed27c52780b4b28aca41ca357cdc8b1
Asif-packt <35103806+Asif-packt@users.noreply.github.com>
2019-05-06 18:35:13 +0530

    Code Updated

---
+++ lesson64imagesproject/lesson64imageviews/lesson64imageviews/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace lesson64imageviews
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.TreeNode treeNode22 = new System.Windows.Forms.TreeNode("2015");
+            System.Windows.Forms.TreeNode treeNode23 = new System.Windows.Forms.TreeNode("2014");
+            System.Windows.Forms.TreeNode treeNode24 = new System.Windows.Forms.TreeNode("Vacation Photos", new System.Windows.Forms.TreeNode[] {
+            treeNode22,
+            treeNode23});
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.btnForward = new System.Windows.Forms.Button();
+            this.btnBack = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(13, 13);
+            this.treeView1.Name = "treeView1";
+            treeNode22.Name = "Node1";
+            treeNode22.Text = "2015";
+            treeNode23.Name = "Node2";
+            treeNode23.Text = "2014";
+            treeNode24.Name = "Node0";
+            treeNode24.Text = "Vacation Photos";
+            this.treeView1.Nodes.AddRange(new System.Windows.Forms.TreeNode[] {
+            treeNode24});
+            this.treeView1.Size = new System.Drawing.Size(121, 97);
+            this.treeView1.TabIndex = 0;
+            this.treeView1.NodeMouseDoubleClick += new System.Windows.Forms.TreeNodeMouseClickEventHandler(this.treeView1_NodeMouseDoubleClick);
+            // 
+            // btnForward
+            // 
+            this.btnForward.Location = new System.Drawing.Point(141, 13);
+            this.btnForward.Name = "btnForward";
+            this.btnForward.Size = new System.Drawing.Size(75, 23);
+            this.btnForward.TabIndex = 1;
+            this.btnForward.Text = "Forward";
+            this.btnForward.UseVisualStyleBackColor = true;
+            this.btnForward.Click += new System.EventHandler(this.btnForward_Click);
+            // 
+            // btnBack
+            // 
+            this.btnBack.Location = new System.Drawing.Point(141, 43);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(75, 23);
+            this.btnBack.TabIndex = 2;
+            this.btnBack.Text = "Back";
+            this.btnBack.UseVisualStyleBackColor = true;
+            this.btnBack.Click += new System.EventHandler(this.btnBack_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(13, 128);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(694, 600);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.Filter = "Image Files (*.jpg)|*.jpg";
+            // 
+            // imageList1
+            // 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth32Bit;
+            this.imageList1.ImageSize = new System.Drawing.Size(255, 255);
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(732, 751);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnBack);
+            this.Controls.Add(this.btnForward);
+            this.Controls.Add(this.treeView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.Button btnForward;
+        private System.Windows.Forms.Button btnBack;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ImageList imageList1;
+    }
+}
+
---
+++ lesson64imagesproject/lesson64imageviews/lesson64imageviews/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lesson64imageviews
+{
+    public partial class Form1 : Form
+    {
+        private int currentIndex = 0;//variable used for storing position of image list, set equal to 0 at first because of the image list
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void treeView1_NodeMouseDoubleClick(object sender, TreeNodeMouseClickEventArgs e)
+        {
+            imageList1.Images.Clear();//clear image list whenever a new year is chosen
+            openFileDialog1.Multiselect = true;//this allows us to select multiple images
+            if(openFileDialog1.ShowDialog()==DialogResult.OK)
+            {
+                for(int i=0;i<openFileDialog1.FileNames.Length;i++)//needed to grab each image from the open file dialog array of images
+                {
+                    imageList1.Images.Add(Image.FromFile(openFileDialog1.FileNames[i]));//makes images from the images array the file dialog is providing
+                }
+                pictureBox1.Image = imageList1.Images[currentIndex];//make sure the picture box displays an image after the dialog box is closed
+            }
+        }
+
+        private void btnForward_Click(object sender, EventArgs e)
+        {
+            //make sure that you don't step beyond the last index of the
+            //makes sure that some images have actually been added
+            if(currentIndex!=imageList1.Images.Count-1 && imageList1.Images.Count>0)
+            {
+                pictureBox1.Image = imageList1.Images[++currentIndex];//first add 1 to the index and then display that new picture in the picture box
+            }
+        }
+
+        private void btnBack_Click(object sender, EventArgs e)
+        {
+            if (currentIndex != 0) //make sure the image index is not at 0
+                pictureBox1.Image = imageList1.Images[--currentIndex];//decrease index by 1 and display that updated image to picture box 
+        }
+    }
+}
